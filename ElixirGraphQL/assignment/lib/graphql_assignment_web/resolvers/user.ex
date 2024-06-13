257f7504b98974a16790fa5f23066838a50b37de
Victoria Argañaras <varganaras@cuentamono.com>
2024-06-13 15:45:13 -0300

    Fix get by name

--- ElixirGraphQL/assignment/lib/graphql_assignment_web/resolvers/user.ex
+++ ElixirGraphQL/assignment/lib/graphql_assignment_web/resolvers/user.ex
@@ -10,7 +10,7 @@ defmodule GraphqlAssignmentWeb.Resolvers.User do
     User.get_by_preferences(preferences)
   end
 
-  def get_by_name(name, _), do: User.get_by_name(name)
+  def get_by_name(%{name: name}, _), do: User.get_by_name(name)
 
   def create(%{id: id, name: _name, email: _email, preferences: _preferences} = params, _) do
     id = String.to_integer(id)
