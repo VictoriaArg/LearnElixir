5f9e988656d283a3209831250ded456e9f31a987
Victoria Argañaras <varganaras@cuentamono.com>
2024-08-21 21:27:21 -0300

    Fix or

--- ElixirGraphQL/assignment/config/config.exs
+++ ElixirGraphQL/assignment/config/config.exs
@@ -10,8 +10,9 @@ import Config
 config :graphql_assignment, GraphqlAssignment.Repo,
   database: "graphql_assignment_repo",
   username: "postgres",
-  password: System.get_env("DB_PASSWORD") | "postgres",
-  hostname: "localhost"
+  password: System.get_env("DB_PASSWORD") || "postgres",
+  hostname: "localhost",
+    show_sensitive_data_on_connection_error: true
 
   config :graphql_assignment,
       ecto_repos: [GraphqlAssignment.Repo]
